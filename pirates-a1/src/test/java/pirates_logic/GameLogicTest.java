b9e00fed2e05ff896adcdf9fcfedc7fa1ee4d322
Amrhe-Minott <60018839+Amrhe-Minott@users.noreply.github.com>
2022-09-23 16:28:31 -0400

    U-TEST test now handles doubling for captain card effect

--- pirates-a1/src/test/java/pirates_logic/GameLogicTest.java
+++ pirates-a1/src/test/java/pirates_logic/GameLogicTest.java
@@ -35,7 +35,7 @@ public class GameLogicTest {
 		Captain captain = new Captain();
 		ArrayList<String> dice = new ArrayList<>(Arrays.asList(DieSides.SKULL, DieSides.SKULL, DieSides.PARROT,
 				DieSides.MONKEY, DieSides.DIAMOND, DieSides.DIAMOND, DieSides.SWORD, DieSides.MONKEY));
-		assertEquals(gameLogic.scoreTurn(dice, captain), 200);
+		assertEquals(gameLogic.scoreTurn(dice, captain), 400);
 	}
 	
 	
