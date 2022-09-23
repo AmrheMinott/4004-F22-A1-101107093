afdb462fd066fd75c300a4fb0b269752cd2008db
Amrhe-Minott <60018839+Amrhe-Minott@users.noreply.github.com>
2022-09-23 17:33:52 -0400

    CODE handle of a kind test

--- pirates-a1/src/main/java/pirates_logic/GameLogic.java
+++ pirates-a1/src/main/java/pirates_logic/GameLogic.java
@@ -1,7 +1,10 @@
 package pirates_logic;
 
+import static java.util.Map.entry;
+
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 import constants.DieSides;
 import fortune_cards.Captain;
@@ -14,6 +17,8 @@ import fortune_cards.FortuneCard;
  */
 public class GameLogic {
 
+	private Map<Integer, Integer> ofAKindScoreMap = Map.ofEntries(entry(3, 100), entry(4, 200), entry(5, 500),
+			entry(6, 1000), entry(7, 2000), entry(8, 4000));
 	private HashMap<String, Integer> diceSideMap = new HashMap<String, Integer>();
 	private final int MAX_NUMBER_OF_SKULLS = 3;
 	private final int DIAMOND_AND_GOLD_MULTIPLIER = 100;
@@ -33,10 +38,17 @@ public class GameLogic {
 		if (diceSideMap.get(DieSides.SKULL) >= MAX_NUMBER_OF_SKULLS) {
 			return 0;
 		}
+
+		int final_score = 0;
+		for (Integer value : diceSideMap.values()) {
+			if (ofAKindScoreMap.get(value) != null) {
+				final_score += ofAKindScoreMap.get(value).intValue();
+			}
+		}
 		int POINTS_DIAMOND_COIN = diceSideMap.get(DieSides.DIAMOND) * DIAMOND_AND_GOLD_MULTIPLIER;
 		int POINTS_GOLD_COIN = diceSideMap.get(DieSides.GOLD) * DIAMOND_AND_GOLD_MULTIPLIER;
-		
-		int final_score = POINTS_DIAMOND_COIN + POINTS_GOLD_COIN;
+
+		final_score += (POINTS_DIAMOND_COIN + POINTS_GOLD_COIN);
 		if (card instanceof Captain) {
 			final_score = final_score * 2;
 		}
