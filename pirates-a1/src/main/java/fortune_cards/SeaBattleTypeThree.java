62ce182f1f912415e8dc0d91ba3ed6efb2851541
Amrhe-Minott <60018839+Amrhe-Minott@users.noreply.github.com>
2022-09-29 19:50:43 -0400

    CODE added code for thrid type of card for sa battle

---
+++ pirates-a1/src/main/java/fortune_cards/SeaBattleTypeThree.java
@@ -0,0 +1,15 @@
+package fortune_cards;
+
+public class SeaBattleTypeThree extends SeaBattle {
+
+	@Override
+	public int getAdditionalPoints() {
+		return 1000;
+	}
+
+	@Override
+	public int getRequiredNumberOfSwords() {
+		return 4;
+	}
+
+}
