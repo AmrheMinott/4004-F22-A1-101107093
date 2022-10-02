53587ed0d7e71aefb2db4ce2e4b81a781b5b56c7
Amrhe-Minott <60018839+Amrhe-Minott@users.noreply.github.com>
2022-10-02 08:40:49 -0400

    A-TEST handling ROW 52 with gold coin and diamond

--- pirates-a1/src/test/java/pirates_logic/PiratesCustomerAcceptanceTests.java
+++ pirates-a1/src/test/java/pirates_logic/PiratesCustomerAcceptanceTests.java
@@ -8,6 +8,7 @@ import java.util.Arrays;
 import org.junit.jupiter.api.Test;
 
 import constants.DieSides;
+import fortune_cards.Captain;
 import fortune_cards.GoldCard;
 import player.Player;
 
@@ -19,6 +20,7 @@ public class PiratesCustomerAcceptanceTests {
             DieSides.NONE, DieSides.NONE, DieSides.NONE, DieSides.NONE, DieSides.NONE));
 
     private GoldCard coin = new GoldCard();
+    private Captain captain = new Captain();
 
     /**
      * Row 45 of SpreadSheet
@@ -119,5 +121,17 @@ public class PiratesCustomerAcceptanceTests {
                 DieSides.GOLD, DieSides.GOLD, DieSides.GOLD, DieSides.GOLD, DieSides.GOLD, DieSides.GOLD));
 
         assertEquals(gameLogic.scoreTurn(player.getRoll(), player.getFortuneCard()), 4800);
+    }
+
+    @Test
+    public void row52() {
+        player.setFortuneCard(captain);
+        player.setRoll(dieRolled);
+        gameLogic.rollAllEightDie(dieRolled);
+
+        player.setRoll(new ArrayList<>(Arrays.asList(DieSides.SKULL, DieSides.PARROT,
+                DieSides.PARROT, DieSides.GOLD, DieSides.GOLD, DieSides.DIAMOND, DieSides.DIAMOND, DieSides.SWORD)));
+
+        assertEquals(800, gameLogic.scoreTurn(player.getRoll(), player.getFortuneCard()));
     }
 }
