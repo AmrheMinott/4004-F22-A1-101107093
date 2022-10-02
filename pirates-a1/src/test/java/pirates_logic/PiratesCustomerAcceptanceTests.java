8a5c917e99db5ebf141dac0a4aae65ead0f53fed
Amrhe-Minott <60018839+Amrhe-Minott@users.noreply.github.com>
2022-10-01 21:43:17 -0400

    A-TEST handle user rolled 1 skull then rerolls 2 more skulls and DIES

--- pirates-a1/src/test/java/pirates_logic/PiratesCustomerAcceptanceTests.java
+++ pirates-a1/src/test/java/pirates_logic/PiratesCustomerAcceptanceTests.java
@@ -17,7 +17,7 @@ public class PiratesCustomerAcceptanceTests {
     private GameLogic gameLogic = new GameLogic();
     private ArrayList<String> dieRolled = new ArrayList<>(Arrays.asList(DieSides.NONE, DieSides.NONE, DieSides.NONE,
             DieSides.NONE, DieSides.NONE, DieSides.NONE, DieSides.NONE, DieSides.NONE));
-    
+
     private GoldCard coin = new GoldCard();
 
     /**
@@ -31,6 +31,22 @@ public class PiratesCustomerAcceptanceTests {
 
         player.setRoll(new ArrayList<>(Arrays.asList(DieSides.SKULL, DieSides.SKULL, DieSides.SKULL,
                 DieSides.MONKEY, DieSides.MONKEY, DieSides.MONKEY, DieSides.MONKEY, DieSides.MONKEY)));
+
+        assertEquals(gameLogic.scoreTurn(player.getRoll(), coin), 0);
+    }
+
+    @Test
+    public void row46() {
+
+        player.setRoll(dieRolled);
+        gameLogic.rollAllEightDie(dieRolled);
+
+        player.setRoll(new ArrayList<>(Arrays.asList(DieSides.SKULL, DieSides.PARROT, DieSides.PARROT,
+                DieSides.PARROT, DieSides.PARROT, DieSides.SWORD, DieSides.SWORD, DieSides.SWORD)));
+
+        player.reRollAtIndex(5, DieSides.SKULL);
+        player.reRollAtIndex(6, DieSides.SKULL);
+        player.reRollAtIndex(7, DieSides.SWORD);
 
         assertEquals(gameLogic.scoreTurn(player.getRoll(), coin), 0);
     }
