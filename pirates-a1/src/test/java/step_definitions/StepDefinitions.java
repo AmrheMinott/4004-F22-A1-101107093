016d1b8e12753a1600fc55ff0bb577fa3bca6dda
Amrhe-Minott <60018839+Amrhe-Minott@users.noreply.github.com>
2022-10-31 12:07:35 -0400

    A-TEST added row 46

--- pirates-a1/src/test/java/step_definitions/StepDefinitions.java
+++ pirates-a1/src/test/java/step_definitions/StepDefinitions.java
@@ -51,6 +51,67 @@ public class StepDefinitions {
         roll = tempRoll;
     }
 
+    @When("player rolls {int} {string}, {int} {string} and {int} {string}")
+    public void player_rolls_and(Integer int1, String string, Integer int2, String string2, Integer int3,
+            String string3) {
+        ArrayList<String> tempRoll = new ArrayList<String>();
+        for (int i = 0; i < int1; i++) {
+            tempRoll.add(string);
+        }
+        for (int i = 0; i < int2; i++) {
+            tempRoll.add(string2);
+        }
+        for (int i = 0; i < int3; i++) {
+            tempRoll.add(string3);
+        }
+        roll = tempRoll;
+    }
+
+    @Then("player gets {int} {string}")
+    public void player_gets(Integer int1, String string) {
+        for (int i = 0; i < roll.size(); i++) {
+            if (int1 == 0) {
+                break;
+            }
+            if (roll.get(i) == "-") {
+                roll.set(i, string);
+                int1--;
+            }
+        }
+    }
+
+    @When("player reroll {int} {string}")
+    public void player_reroll(Integer int1, String string) {
+        for (int i = 0; i < roll.size(); i++) {
+            if (roll.get(i).equals(string)) {
+                roll.set(i, "-");
+            }
+        }
+    }
+
+    @Then("player gets {int} {string} and {int} {string} after reroll")
+    public void player_gets_and_after_reroll(Integer int1, String string, Integer int2, String string2) {
+        for (int i = 0; i < roll.size(); i++) {
+            if (int1 == 0) {
+                break;
+            }
+            if (roll.get(i) == "-") {
+                roll.set(i, string);
+                int1--;
+            }
+        }
+
+        for (int i = 0; i < roll.size(); i++) {
+            if (int2 == 0) {
+                break;
+            }
+            if (roll.get(i) == "-") {
+                roll.set(i, string2);
+                int2--;
+            }
+        }
+    }
+
     @Then("player scores {int} after Death")
     public void player_scores_after_death(Integer int1) {
         assertEquals(int1, gameLogic.scoreTurn(roll, card));
--- pirates-a1/src/test/resources/acceptance_tests/AcceptanceTest.feature
+++ pirates-a1/src/test/resources/acceptance_tests/AcceptanceTest.feature
@@ -4,4 +4,10 @@ Feature: Acceptance Tests for Assignment 2
     Given Fortune Card as "Coin"
     When player rolls 3 "Skull" and 5 "Sword"
     Then player scores 0 after Death
-    
+
+  Scenario: Row 46
+    Given Fortune Card as "Coin"
+    When player rolls 1 "Skull", 4 "Parrot" and 3 "Sword"
+    When player reroll 3 "Sword"
+    Then player gets 2 "Skull" and 1 "Sword" after reroll
+    And player scores 0 after Death
