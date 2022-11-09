ce9b93cf14ce8e10cac3ef6d73c3e7dad0d426b2
Amrhe-Minott <60018839+Amrhe-Minott@users.noreply.github.com>
2022-11-09 09:27:17 -0500

    REFACTOR added print statement for some reason this is needed
    and removed Multiplayer case from this feature file

--- pirates-a1/src/test/java/step_definitions/StepDefinitions.java
+++ pirates-a1/src/test/java/step_definitions/StepDefinitions.java
@@ -2,6 +2,7 @@ package step_definitions;
 
 import static java.util.Map.entry;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -38,13 +39,12 @@ public class StepDefinitions {
     private FortuneCard card = null;
     private GameLogic gameLogic = new GameLogic();
     private Player player_1 = new Player("Cumcumber 1");
-    private Player player_2 = new Player("Cumcumber 2");
-    private Player player_3 = new Player("Cumcumber 3");
 
-    private ArrayList<Player> players = new ArrayList<Player>(Arrays.asList(player_1, player_2, player_3));
+    private ArrayList<Player> players = new ArrayList<Player>(Arrays.asList(player_1));
 
     @Given("player {int} Fortune Card as {string}")
     public void player_fortune_card_as(Integer playerIndex, String cardString) {
+        System.out.println("Player was granted Fortune Card -> " + cardString);
         card = fortuneCardMap.get(cardString);
         players.get(playerIndex - 1).setFortuneCard(card);
     }
@@ -55,7 +55,10 @@ public class StepDefinitions {
         for (int i = 0; i < int1; i++) {
             tempRoll.add(string);
         }
+        gameLogic.rollAllEightDie(players.get(playerIndex - 1).getRoll());
+        System.out.println("Player roll after simulation -> " + players.get(playerIndex - 1).getRoll().toString());
         players.get(playerIndex - 1).setRoll(tempRoll);
+        System.out.println("Player roll set to -> " + players.get(playerIndex - 1).getRoll().toString());
     }
 
     @When("player {int} rolls {int} {string} and {int} {string}")
@@ -67,8 +70,11 @@ public class StepDefinitions {
         for (int i = 0; i < int2; i++) {
             tempRoll.add(string2);
         }
+        gameLogic.rollAllEightDie(players.get(playerIndex - 1).getRoll());
+        System.out.println("Player roll after simulation -> " + players.get(playerIndex - 1).getRoll().toString());
         addSkullBasedOnSkullCard(tempRoll);
         players.get(playerIndex - 1).setRoll(tempRoll);
+        System.out.println("Player roll set to -> " + players.get(playerIndex - 1).getRoll().toString());
     }
 
     private void addSkullBasedOnSkullCard(ArrayList<String> tempRoll) {
@@ -95,8 +101,11 @@ public class StepDefinitions {
         for (int i = 0; i < int3; i++) {
             tempRoll.add(string3);
         }
+        gameLogic.rollAllEightDie(players.get(playerIndex - 1).getRoll());
+        System.out.println("Player roll after simulation -> " + players.get(playerIndex - 1).getRoll().toString());
         addSkullBasedOnSkullCard(tempRoll);
         players.get(playerIndex - 1).setRoll(tempRoll);
+        System.out.println("Player roll set to -> " + players.get(playerIndex - 1).getRoll().toString());
     }
 
     @When("player {int} rolls {int} {string}, {int} {string}, {int} {string} and {int} {string}")
@@ -116,7 +125,10 @@ public class StepDefinitions {
         for (int i = 0; i < int4; i++) {
             tempRoll.add(string4);
         }
+        gameLogic.rollAllEightDie(players.get(playerIndex - 1).getRoll());
+        System.out.println("Player roll after simulation -> " + players.get(playerIndex - 1).getRoll().toString());
         players.get(playerIndex - 1).setRoll(tempRoll);
+        System.out.println("Player roll set to -> " + players.get(playerIndex - 1).getRoll().toString());
     }
 
     @When("player {int} rolls {int} {string}, {int} {string}, {int} {string}, {int} {string} and {int} {string}")
@@ -139,7 +151,10 @@ public class StepDefinitions {
         for (int i = 0; i < int5; i++) {
             tempRoll.add(string5);
         }
+        gameLogic.rollAllEightDie(players.get(playerIndex - 1).getRoll());
+        System.out.println("Player roll after simulation -> " + players.get(playerIndex - 1).getRoll().toString());
         players.get(playerIndex - 1).setRoll(tempRoll);
+        System.out.println("Player roll set to -> " + players.get(playerIndex - 1).getRoll().toString());
     }
 
     @Then("player {int} gets {int} {string}")
@@ -153,12 +168,15 @@ public class StepDefinitions {
                 int1--;
             }
         }
+        System.out
+                .println("Player roll after reroll is set to -> " + players.get(playerIndex - 1).getRoll().toString());
     }
 
     @When("player {int} reroll {int} {string}")
     public void player_reroll(Integer playerIndex, Integer int1, String string) {
         for (int i = 0; i < players.get(playerIndex - 1).getRoll().size(); i++) {
             if (players.get(playerIndex - 1).getRoll().get(i).equals(string)) {
+                gameLogic.rollDiePair(i + 1, i + 1, players.get(playerIndex - 1).getRoll());
                 players.get(playerIndex - 1).getRoll().set(i, "-");
             }
         }
@@ -175,6 +193,8 @@ public class StepDefinitions {
                 int1--;
             }
         }
+        System.out
+                .println("Player roll after reroll is set to -> " + players.get(playerIndex - 1).getRoll().toString());
     }
 
     @Then("player {int} gets {int} {string} and {int} {string} after reroll")
@@ -199,6 +219,8 @@ public class StepDefinitions {
                 int2--;
             }
         }
+        System.out
+                .println("Player roll after reroll is set to -> " + players.get(playerIndex - 1).getRoll().toString());
     }
 
     @Then("player {int} gets {int} {string}, {int} {string} and {int} {string} after reroll")
@@ -233,19 +255,26 @@ public class StepDefinitions {
                 int3--;
             }
         }
+        System.out
+                .println("Player roll after reroll is set to -> " + players.get(playerIndex - 1).getRoll().toString());
     }
 
     @Then("player {int} scores {int} after Death")
     public void player_scores_after_death(Integer playerIndex, Integer int1) {
         assertEquals(int1, gameLogic.scoreTurn(players.get(playerIndex - 1).getRoll(),
                 players.get(playerIndex - 1).getFortuneCard()));
+        boolean isPlayerDead = players.get(playerIndex - 1).isPlayerDead();
+        System.out.println("Player Died -> " + Boolean.toString(isPlayerDead));
+        assertTrue(isPlayerDead);
     }
 
     @Then("player {int} scores {int}")
     public void player_scores(Integer playerIndex, Integer int1) {
         int score = gameLogic.scoreTurn(players.get(playerIndex - 1).getRoll(),
                 players.get(playerIndex - 1).getFortuneCard());
+        System.out.println("Player earned " + Integer.toString(score));
         players.get(playerIndex - 1).incrementScore(score);
+        System.out.println("Player new score = " + players.get(playerIndex - 1).getScore());
         assertEquals(int1, score);
     }
 
@@ -256,13 +285,15 @@ public class StepDefinitions {
         players.get(playerIndex - 1).incrementScore(score);
         assertEquals(int1, score);
     }
-    
+
     @When("Game declares {string} as winner")
     public void game_declares_as_winner(String string) {
         HashMap<String, Integer> map = new HashMap<String, Integer>();
         for (Player p : players) {
             map.put(p.getName(), p.getScore());
         }
+        String winner = gameLogic.determineWinner(map);
+        System.out.println("The winner in the end is -> " + winner);
         assertEquals(string, gameLogic.determineWinner(map));
     }
 
@@ -277,6 +308,7 @@ public class StepDefinitions {
     public void player_activates_sorceress_getting_a(Integer playerIndex, String string) {
         for (int i = 0; i < players.get(playerIndex - 1).getRoll().size(); i++) {
             if (players.get(playerIndex - 1).getRoll().get(i).equals("Skull")) {
+                players.get(playerIndex - 1).sorceressActivation();
                 players.get(playerIndex - 1).getRoll().set(i, string);
                 break;
             }
